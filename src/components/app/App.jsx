266a59b8bce3fbc92c56ac61f969c64996b3b611
Kyle Mayer <x@kylemayer.dev>
2021-09-08 15:58:04 -0700

    file structure and list page renders

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import ListPage from '../../containers/ListPage';
+import DetailsPage from '../../containers/DetailsPage';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <h1>Animal Crossing API Fetch</h1>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={routerProps => <ListPage {...routerProps}/>}
+        />
+        <Route
+          path="/:villager"
+          exact
+          render={routerProps => <DetailsPage {...routerProps}/>}
+        />
+      </Switch>
+    </Router>
+  );
 }
