77752a0dc28169edbc443a137a66bf489aa2835b
Kyle Mayer <x@kylemayer.dev>
2021-09-10 15:00:43 -0700

    wrapped listpage in memory router tag for test to pass

--- src/containers/ListPage.test.jsx
+++ src/containers/ListPage.test.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import ListPage from './ListPage';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('List Page', () => {
-
+  afterEach(() => cleanup());
   it('should render the List page', async () => {
-    render(<ListPage />);
+    render(
+      <MemoryRouter>
+        <ListPage />
+      </MemoryRouter>
+    );
 
-    const ul = await screen.findByRole('villagers', { name: 'villagers' });
+    const ul = await screen.findByRole('list', { name: 'villagers' });
     expect(ul).toBeInTheDocument();
     expect(ul).toMatchSnapshot();
   });
